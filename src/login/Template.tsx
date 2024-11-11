b50be13f8801ace2902cda302e9c79ef63a58909
Balazs Nobik <balazsnobik@gmail.com>
2024-11-11 21:11:16 +0100

    logo changed

--- src/login/Template.tsx
+++ src/login/Template.tsx
@@ -7,7 +7,7 @@ import { useSetClassName } from "keycloakify/tools/useSetClassName";
 import { useInitialize } from "keycloakify/login/Template.useInitialize";
 import type { I18n } from "./i18n";
 import type { KcContext } from "./KcContext";
-import fideLogo from "./assets/fideLogo_rgb.png";
+import fideLogo from "./assets/fideCoverBackgroundOverlay.jpg";
 
 export default function Template(props: TemplateProps<KcContext, I18n>) {
     const {
